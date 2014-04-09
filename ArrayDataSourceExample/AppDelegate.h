d008c03b5ad0a9bfe6cfe7e68289206a511f2c0f
Angel Casado <angel@casado.me>
2014-04-09 14:07:57 -0400

    Initial Commit

---
+++ ArrayDataSourceExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArrayDataSourceExample
+//
+//  Created by Angel on 4/9/14.
+//  Copyright (c) 2014 Angel Casado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ArrayDataSourceExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ArrayDataSourceExample
+//
+//  Created by Angel on 4/9/14.
+//  Copyright (c) 2014 Angel Casado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
